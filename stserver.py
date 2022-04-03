d77c8d5f098111a92f9a28ef55fb55ace3262833
rahul717-hero <59031495+rahul717-hero@users.noreply.github.com>
2022-04-03 15:27:32 +0530

    Update stserver.py

--- stserver.py
+++ stserver.py
@@ -3,8 +3,6 @@ import pandas as pd
 import numpy as np
 import subprocess
 import sys
-subprocess.run(["playwright","install"])
-subprocess.run(["playwright","install-deps"])
 subprocess.run([f"{sys.executable}","cpgrm.py"])
 subprocess.run([f"{sys.executable}","scrapCM.py"])
 subprocess.run([f"{sys.executable}","scrapcgrs.py"])
